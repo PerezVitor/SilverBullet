24b499893f64315eaaa03a700f1e3e3d7cb891aa
PerezVitor <vitor.g.perez@hotmail.com>
2023-01-10 23:27:11 -0300

    Primeiro commit - Projeto silver bullet

---
+++ src/Web.Mvc.Site/Controllers/CalculatorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web.Mvc.Site.Models;
+
+namespace Web.Mvc.Site.Controllers;
+public class CalculatorController : Controller
+{
+    public IActionResult Index() => View();
+
+    public void Operacao(string teste)
+    {
+        _ = teste;
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
